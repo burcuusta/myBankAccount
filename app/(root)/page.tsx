091f571e8da0e7f9a023f17e6f057832da639cef
burcuusta <burcuusta93@gmail.com>
2025-02-01 20:09:40 +0300

    header düzenlemesi
    header için ilk düzenleme yapıldı

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
 
-const Home = () => {
+const Home: React.FC = () => {
+	const loggedIn = {firstName: 'Adrian'};
+
+
   return (
-	<div>
-	  Home
-	</div>
+	<section className='home'>
+		<div className='home-content'>
+			<header className='home-header'>
+				<HeaderBox 
+				type="greeting"
+				title="Welcome"
+				user={loggedIn?.firstName || 'Guest'}
+				subtext="Access and manage your account and transactions efficiently."
+				/>
+			</header>
+		</div>
+	</section>
   );
 };
 
